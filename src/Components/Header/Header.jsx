769868e34df6c56c640e5658dec6b41ba793ff7c
Lewis Murray <lewisdmurray00@gmail.com>
2023-04-10 22:15:26 +0100

    Added some jsx to create the header section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,26 @@ import { AppWrap } from '../../wrapper'
 const Header = () => {
   return (
     <div className='app__header app__flex'>
-        Headers
+        <div className='app__header-info'>
+            
+            <div className='app__header-badge'>
+                <div className='badge-cmp app__flex'>
+                    <span>🛠️</span>
+                    <div style={{ marginLeft: 20}}>
+                        <h1 className='head-text'>WeBuild</h1>
+                    </div>
+                </div>
+
+                <div className='tag-cmp app__flex'>
+                    <p className='p-text'>Plumbing and Gas</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div>
+            
+        </div>
     </div>
   )
 }
